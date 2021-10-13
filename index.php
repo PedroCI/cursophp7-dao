3233d7a747863f4d8993cde83f7391a10b1d3142
Pedro C. I <pedro_igreja@icloud.com>
2021-10-13 17:33:52 -0300

    metodos de lista

--- index.php
+++ index.php
@@ -1,12 +1,28 @@
 <?php 
 
 require_once("config.php");
-
-$root = new Usuario();
+//carrega um usuario
+/*$root = new Usuario();
 
 $root->loadById(3);
 
-echo $root;
+echo $root;*/
+
+//Carrega uma lista de usuarios
+/*$lista = Usuario::getList();
+
+echo json_encode($lista);*/
+
+//Carrega uma lista de usuarios buscandoo pelo login
+/*$search = Usuario::search("jo");
+
+echo json_encode($search);*/
+
+//carrega um usuario usando login e senha
+$usuario = new Usuario();
+$usuario->login("root", "!@#$");
+
+echo $usuario;
 
 /*$sql= new Sql();
 
